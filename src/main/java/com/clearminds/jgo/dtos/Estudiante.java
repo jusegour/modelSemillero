83ce98ab700ffa2d5c70fbbf735abb73de90aa67
jusegour <jusegour2015@gmail.com>
2020-12-09 10:44:10 -0500

    Version Inicial

---
+++ src/main/java/com/clearminds/jgo/dtos/Estudiante.java
@@ -0,0 +1,51 @@
+package com.clearminds.jgo.dtos;
+
+public class Estudiante {
+	
+	private String nombre;
+	private String apellido;
+	private int id;
+	
+	public Estudiante(String nombre,String apellido,int id){
+		this.nombre=nombre;
+		this.apellido=apellido;
+		this.id=id;
+	}
+	
+	public Estudiante(String nombre,String apellido){
+		this.nombre=nombre;
+		this.apellido=apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+	}
+	
+	
+
+}
